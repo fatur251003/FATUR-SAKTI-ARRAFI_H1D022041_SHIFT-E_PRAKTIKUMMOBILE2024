e0021a80746a4924e4892c3cb82777f055b8a55b
fatur251003 <minaritwice2@gmail.com>
2024-09-08 11:39:47 +0700

    Add files via upload

--- Pertemuan1/source_code.dart
+++ Pertemuan1/source_code.dart
@@ -1,1 +1,26 @@
+import 'package:pertemuan1/pertemuan1.dart' as pertemuan1;
 
+void main() {
+  int umur = 21;
+  double tinggi = 177;
+  String nama = "Fatur Sakti Arrafi";
+  bool isStudent = true;
+
+  int hasilPenjumlahan = umur + 5;
+  double hasilPembagian = tinggi / 2;
+
+  if (isStudent) {
+    print("$nama adalah seorang Mahasiswa Informatika 2022.");
+  } else {
+    print("$nama bukan seorang Mahasiswa Informatika 2022.");
+  }
+
+  void sapa(String nama) {
+    print("Halo, $nama!");
+  }
+
+  sapa("Fatur Sakti Arrafi");
+
+  String pesan = "Umur saya adalah $umur tahun dan tinggi saya $tinggi cm.";
+  print(pesan);
+}
